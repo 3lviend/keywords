2be90a6de65cb1441f7a2a06567f1355915c49ef
3lviend <henryss_10@yahoo.com>
2015-10-16 11:52:02 +0700

    fixed import csv and migration

--- db/migrate/20151015163250_create_keywords.rb
+++ db/migrate/20151015163250_create_keywords.rb
@@ -5,7 +5,7 @@ class CreateKeywords < ActiveRecord::Migration
       t.integer :impressions, default: 0, null: false
       t.integer :clicks, default: 0, null: false
       t.decimal :ctr, precision: 4, scale: 2
-      t.decimal :avg_position, precision: 4, scale: 2
+      t.decimal :avg_position, precision: 10, scale: 2
       t.date :date
       t.boolean :new, default: true
       t.timestamps null: false
